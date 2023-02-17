db45843951f4ee788b3f58a8546782a3058c8198
MolefiModula <molefimodula@hotmail.com>
2023-02-17 14:57:01 -0800

    Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <sdio.h>
+
+/**
+ * main - Prints the alphabet in lower casee
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a';ch <= ; 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
