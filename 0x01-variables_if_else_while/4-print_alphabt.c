95fa23234695eb53f04aab07e5f637ce1d1ab04c
MolefiModula <molefimodula@hotmail.com>
2023-02-17 19:35:32 -0800

    A program that prints the alphabet in lowercase, followerd by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Followed by a new line, exvept q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
