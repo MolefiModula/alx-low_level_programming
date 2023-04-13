502f25c081759dffd0f4545382b5c12f685f568b
MolefiModula <molefimodula@hotmail.com>
2023-04-12 19:48:15 -0700

    updated putchar and main file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-#include <stdio.h>
-#include <string.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int binary_to_uint(const char *binary_string);
 void print_binary(unsigned long int num);
@@ -10,8 +7,8 @@ int get_bit_at_index(unsigned long int num, unsigned int index);
 int set_bit_at_index(unsigned long int *num, unsigned int index);
 int clear_bit_at_index(unsigned long int *num, unsigned int index);
 unsigned int count_flipped_bits(unsigned long int num1, unsigned long intnum2);
-int_atoi(const char*s);
-int_putchar(char c);
+int _atoi(const char*s);
+int _putchar(char c);
 int get_endianness(void);
 
 #endif
