1b108cdc727b66d4d6b598e51acafb9f31ccdc9c
MolefiModula <molefimodula@hotmail.com>
2023-04-12 20:04:02 -0700

    updated files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,18 @@
 unsigned int binary_to_uint(const char *binary_string)
 {
 	unsigned int result = 0;
-
-	if (b == NULL)
+	while (*binary_string)
+	{
+	if (binary_string == NULL)
 		return 0;
 
-	for(; *binary_string; b++)
+	for(; *binary_string; binary_string++)
 	{
 		if (binary_string* != '0' && *binary_string != '1')
 			return 0;
 		result = (result << 1) + (*binary_string - '0');
+		binary_string++;
+	}
 	}
 	return result;
 }
