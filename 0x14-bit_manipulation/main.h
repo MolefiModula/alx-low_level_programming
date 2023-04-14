6b880b73f1ed2268f89e1ddca177e2afd1c0116e
MolefiModula <molefimodula@hotmail.com>
2023-04-14 11:28:38 -0700

    updaated main file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@ void print_binary(unsigned long int n);
 int get_bit_at_index(unsigned long int n, unsigned int index);
 int set_bit_at_index(unsigned long int *n, unsigned int index);
 int clear_bit_at_index(unsigned long int *n, unsigned int index);
-unsigned int count_flipped_bits(unsigned long int n, unsigned long int m);
+unsigned int count_flipped_bits(unsigned long int num1, unsigned long int num2);
 int _atoi(const char*s);
 int _putchar(char c);
 int get_endianness(void);
