520cc8ce5b60f5439f27a485cc2a26c0f2bd609a
MolefiModula <molefimodula@hotmail.com>
2023-05-06 05:42:20 -0700

    modified main file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _atoi(const char *s);
 int _putchar(char c);
